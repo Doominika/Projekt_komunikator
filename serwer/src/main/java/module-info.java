837145aa44f12d5da88d0d73dd5c9693d5e62f72
Doominika <101605588+Doominika@users.noreply.github.com>
2022-09-13 19:09:28 +0200

    Delete serwer directory

--- serwer/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.serwer_witt {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.serwer_witt to javafx.fxml;
-    exports com.example.serwer_witt;
-}
